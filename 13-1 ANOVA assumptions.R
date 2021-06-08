0f36fbb946abb222fff95eec13df05a19b859d47
Samantha Seals <sseals@uwf.edu>
2021-06-08 10:29:56 -0500

    add assumptions code

---
+++ 13-1 ANOVA assumptions.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(multcomp)
+library(broom)
+
+almost_sas <- function(aov.results){
+  aov_residuals <- residuals(aov.results)
+  par(mfrow=c(2,2))
+  plot(aov.results, which=1)
+  hist(aov_residuals)
+  plot(aov.results, which=2)
+  plot(density(aov_residuals))
+}
+
+strength <- c(15.4, 12.9, 17.2, 16.6, 19.3,
+              17.2, 14.3, 17.6, 21.6, 17.5,
+              5.5,  7.7, 12.2, 11.4, 16.4,
+              11.0, 12.4, 13.5,  8.9,  8.1)
+
+system <- c(rep("Cojet",5), rep("Silistor",5), rep("Cimara",5), rep("Ceramic",5))
+
+data <- tibble(system, strength)
+data <- data %>% mutate (system = factor(system))
+
+m1 <- aov(strength ~ system, data=data)
+
+almost_sas(m1)
+
+#ggsave("/Users/sseals/Desktop/L80fig1.png")
