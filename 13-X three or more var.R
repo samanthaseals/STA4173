3468a24f44e643a237af10b51b0016ea28b97505
Samantha Seals <sseals@uwf.edu>
2021-06-20 15:30:00 -0500

    add BFL code

---
+++ 13-X three or more var.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(car)
+
+strength <- c(15.4, 12.9, 17.2, 16.6, 19.3,
+              17.2, 14.3, 17.6, 21.6, 17.5,
+              5.5,  7.7, 12.2, 11.4, 16.4,
+              11.0, 12.4, 13.5,  8.9,  8.1)
+
+system <- c(rep("Cojet",5), rep("Silistor",5), rep("Cimara",5), rep("Ceramic",5))
+
+data <- tibble(system, strength)
+
+data$system <- as_factor(data$system)
+
+leveneTest(strength ~ system, data = data, center = median)
