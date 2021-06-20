eb8c03c74b649a326814a16197936b092592cf3b
Samantha Seals <sseals@uwf.edu>
2021-06-20 13:37:31 -0500

    add code for t test for beta

---
+++ 14-1 t test for beta.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+age <- c(25, 25, 28, 32, 32, 32, 38, 42, 48, 51, 51, 58, 62, 65)
+chol <- c(180, 195, 186, 180, 210, 197, 239, 183, 204, 221, 243, 208, 228, 269)
+
+data <- tibble(age, chol)
+
+summary(lm(chol ~ age, data = data))
+
+data$residual <- resid(lm(chol ~ age, data = data))
+data$residual2 <- data$residual^2
+
+sd(data$age)
