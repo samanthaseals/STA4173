34bd9ff21d29039d77ba5c8bbeda6935838ab9da
Samantha Seals <sseals@uwf.edu>
2021-06-30 11:05:39 -0500

    add data back; code for ranks and sum of ranks

--- 15-7 Kruskal-Wallis.R
+++ 15-7 Kruskal-Wallis.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(pgirmess)
+library(pgirmess) 
 
 almost_sas <- function(aov.results){
   aov_residuals <- residuals(aov.results)
@@ -10,7 +10,9 @@ almost_sas <- function(aov.results){
   plot(density(aov_residuals))
 }
 
-HDL <- c()
+HDL <- c(54, 43, 38, 30, 61, 53, 35, 34, 39, 46, 50, 35,
+         61, 41, 44, 47, 33, 29, 59, 35, 34, 74, 50, 65,
+         44, 65, 62, 53, 51, 49, 49, 42, 35, 44, 37, 38)
 
 age <- c(rep("20 to 29", 12), 
          rep("40 to 49", 12),
@@ -18,9 +20,20 @@ age <- c(rep("20 to 29", 12),
 
 data <- tibble(age, HDL)
 
-m1 <- aov(HDL ~ age, data = data)
-almost_sas(m1)
+# find ranks
+data$rank <- rank(data$HDL, ties.method = "average")
 
+# sum of ranks
+data %>% 
+  group_by(age) %>%
+  summarize_at(vars(rank),
+               list(name = sum))
+
+# check ANOVA assumptions
+almost_sas(lm(HDL ~ age, data = data))
+
+# Kruskal-Wallis
 kruskal.test(HDL ~ age, data = data)
 
+# Kruskal-Wallis posthoc
 kruskalmc(HDL ~ age, data = data)
