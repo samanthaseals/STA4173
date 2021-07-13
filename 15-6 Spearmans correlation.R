344119ebbb70e871944ef07adc10afde4c04e43e
Samantha Seals <sseals@uwf.edu>
2021-07-13 01:07:49 -0500

    add code to check assumption on Pearson's correlation

--- 15-6 Spearmans correlation.R
+++ 15-6 Spearmans correlation.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggpubr)
 
 # create data
 x <- c(100, 102, 103, 101, 105, 100, 99, 105)
@@ -8,3 +9,32 @@ data <- as_tibble(x, y)
 
 # find Spearman's correlation
 cor(x, y, method = c("spearman"))
+
+# scatterplot of data
+ggplot(data, aes(x = x, y = y)) + 
+  geom_point(size=5) +
+  ylab("Distance (yards)") +
+  xlab("Club-Head Speed (mph)") +
+  theme_minimal() +
+  theme(text = element_text(size=20))
+
+# check normality assumption on Pearson's
+clubhead <- ggplot(data, aes(sample = x)) +
+  stat_qq(size=3) +
+  stat_qq_line() +
+  theme_minimal() +
+  xlab("Theoretical") +
+  ylab("Sample") +
+  ggtitle("Club Head Speed (mph)") +
+  theme(text = element_text(size=20))
+
+distance <- ggplot(data, aes(sample = y)) +
+  stat_qq(size=3) +
+  stat_qq_line() +
+  theme_minimal() +
+  xlab("Theoretical") +
+  ylab("Sample") +
+  ggtitle("Distance (yards)") +
+  theme(text = element_text(size=20))
+
+ggarrange(clubhead, distance, ncol=2, nrow=1)
